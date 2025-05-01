dd9462b5317f558b2d397201a29e3ef0144ce18e
anorak-lightman <164287745+anorak-lightman@users.noreply.github.com>
2025-05-01 13:54:41 -0700

    wrote tests for api key and getting stories

--- hw2-app/static/main.test.js
+++ hw2-app/static/main.test.js
@@ -1,4 +1,6 @@
 const fetchKey = require('./testFunctions/fetchKey.js');
+const getSacStories = require('./testFunctions/getSacStories.js');
+const getDavisStories = require('./testFunctions/getDavisStories.js');
 const fetch = require('node-fetch');    
 global.fetch = fetch;
 test('basic sanity check', () => {
@@ -10,3 +12,26 @@ test('fetch api key', async () => {
     expect(apiKey).toBe('rFSzaY1UbSRUSoBOt6lsYpXubk49bygA');
 });
 
+test('get sacramento stories', async () => {
+    const sacStories = await getSacStories(0);
+    expect(sacStories[0]).toBeDefined();
+    expect(sacStories[0].headline.main).toBeDefined();
+    expect(sacStories[0].snippet).toBeDefined();
+    expect(sacStories[0].multimedia).toBeDefined();
+    expect(sacStories[0].multimedia.default.url).toBeDefined();
+    expect(sacStories[0].multimedia.default.width).toBeDefined();
+    expect(sacStories[0].multimedia.default.height).toBeDefined();
+    expect(sacStories[0].web_url).toBeDefined();
+});
+
+test('get davis stories', async () => {
+    const davisStories = await getDavisStories(0);
+    expect(davisStories[0]).toBeDefined();
+    expect(davisStories[0].headline.main).toBeDefined();
+    expect(davisStories[0].snippet).toBeDefined();
+    expect(davisStories[0].multimedia).toBeDefined();
+    expect(davisStories[0].multimedia.default.url).toBeDefined();
+    expect(davisStories[0].multimedia.default.width).toBeDefined();
+    expect(davisStories[0].multimedia.default.height).toBeDefined();
+    expect(davisStories[0].web_url).toBeDefined();
+});
